99c993ea261ebdec9f2987b013e47a1961b8ab83
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-04-14 22:59:32 +0200

    Handle undefined properties

--- src/luftdaten-adapter.ts
+++ src/luftdaten-adapter.ts
@@ -9,58 +9,62 @@ import { Adapter, Device, Property } from 'gateway-addon';
 import fetch from 'node-fetch';
 
 interface Measurement {
-  id: number;
-  sensordatavalues: SensorValue[];
-  sensor: Sensor;
+  id?: number;
+  sensordatavalues?: SensorValue[];
+  sensor?: Sensor;
 }
 
 interface SensorValue {
-  value_type: string;
-  value: string;
+  value_type?: string;
+  value?: string;
 }
 
 interface Sensor {
-  sensor_type: SensorType;
-  id: number;
+  sensor_type?: SensorType;
+  id?: number;
 }
 
 interface SensorType {
-  id: string;
-  manufacturer: string;
-  name: string;
+  id?: string;
+  manufacturer?: string;
+  name?: string;
 }
 
 class Luftdaten extends Device {
-  constructor(adapter: any, manifest: any, measurement: Measurement) {
-    super(adapter, `${measurement?.sensor?.id}`);
+  constructor(adapter: any, id: string, measurement: Measurement) {
+    super(adapter, id);
     this['@context'] = 'https://iot.mozilla.org/schemas/';
-    this.name = measurement?.sensor?.sensor_type?.name;
-    this.description = manifest.description;
+    this.name = measurement?.sensor?.sensor_type?.name || id;
 
-    for (const sensorValue of measurement.sensordatavalues) {
+    for (const sensorValue of measurement.sensordatavalues || []) {
       const propertyName = sensorValue.value_type;
 
-      this.addProperty(propertyName, {
-        type: 'number',
-        title: propertyName,
-        readOnly: true
-      });
+      if (propertyName) {
+        this.addProperty(propertyName, {
+          type: 'number',
+          title: propertyName,
+          readOnly: true
+        });
+      }
     }
   }
 
   public update(measurement: Measurement) {
-    for (const sensorValue of measurement.sensordatavalues) {
+    for (const sensorValue of measurement.sensordatavalues || []) {
       const propertyName = sensorValue.value_type;
-      const property = this.properties.get(propertyName);
 
-      if (property) {
-        if (sensorValue.value) {
-          property.setCachedValueAndNotify(sensorValue.value);
+      if (propertyName) {
+        const property = this.properties.get(propertyName);
+
+        if (property) {
+          if (sensorValue.value) {
+            property.setCachedValueAndNotify(sensorValue.value);
+          } else {
+            console.warn(`Could not find value for property ${propertyName}`);
+          }
         } else {
-          console.warn(`Could not find value for property ${propertyName}`);
+          console.warn(`Could not find property ${propertyName}`);
         }
-      } else {
-        console.warn(`Could not find property ${propertyName}`);
       }
     }
   }
@@ -96,16 +100,19 @@ export class LuftdatenAdapter extends Adapter {
 
     for (const measurement of measurements) {
       let id = measurement?.sensor?.id;
-      let device = this.devicesById[id];
 
-      if (!device) {
-        console.log(`Creating new device for ${id}`);
-        device = new Luftdaten(this, this.manifest, measurement);
-        this.devicesById[id] = device;
-        this.handleDeviceAdded(device);
+      if (id) {
+        let device = this.devicesById[id];
+
+        if (!device) {
+          console.log(`Creating new device for ${id}`);
+          device = new Luftdaten(this, `${id}`, measurement);
+          this.devicesById[id] = device;
+          this.handleDeviceAdded(device);
+        }
+
+        device.update(measurement);
       }
-
-      device.update(measurement);
     }
   }
 
